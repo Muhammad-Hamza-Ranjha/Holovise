eccf157ea89f46bc9aae2c3c2ba8158ebcc1e148
Muhammad-Hamza-Ranjha <ranjhahv04@gmail.com>
2026-07-09 15:16:13 +0500

    mobile fixes removed

--- components/home/ThemedHomepage.tsx
+++ components/home/ThemedHomepage.tsx
@@ -2,20 +2,12 @@
 
 import { useTheme } from "next-themes";
 import { useMounted } from "@/components/hooks/useMounted";
-import { useViewportWidth } from "@/components/hooks/useViewportWidth";
 import { DarkHomepage } from "@/components/home/DarkHomepage";
 import { LightHomepage } from "@/components/home/LightHomepage";
-import { MobileHomepage } from "@/components/home/MobileHomepage";
 
 export function ThemedHomepage() {
   const { theme, resolvedTheme } = useTheme();
   const mounted = useMounted();
-  const viewportWidth = useViewportWidth();
-  const activeTheme = mounted && (theme === "light" || resolvedTheme === "light") ? "light" : "dark";
 
-  if (viewportWidth <= 640) {
-    return <MobileHomepage theme={activeTheme} />;
-  }
-
-  return activeTheme === "light" ? <LightHomepage /> : <DarkHomepage />;
+  return mounted && (theme === "light" || resolvedTheme === "light") ? <LightHomepage /> : <DarkHomepage />;
 }
